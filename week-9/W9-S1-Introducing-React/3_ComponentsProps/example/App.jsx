b5dc83b86c926602c24d171e7fe541747804ec2a
fl1jason <jason@fl1digital.com>
2024-11-04 20:08:16 +0000

    Added W9-S1

---
+++ week-9/W9-S1-Introducing-React/3_ComponentsProps/example/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import Number from './components/Number'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const myName = 'Vite';
+
+  const numbers = [1, 2, 3, 4, 5]
+
+  return (
+    <>
+      <div>
+        My name is {myName}
+        {numbers.map((number) => (
+          <Number key={number} number={number} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
